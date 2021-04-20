cd73e94ec98ef0ef49a0130f1271376e88505512
victor <victor@swipejobs.com>
2021-04-20 15:55:05 +1000

    set up user login and sign up

--- twitter/settings.py
+++ twitter/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    # third party
+    'rest_framework',
+
+    # project app
+    'accounts'
 ]
 
 REST_FRAMEWORK = {
