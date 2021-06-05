cc1dd88f47c882a4fda5c05e697e30679e74857d
aksaneverend <zhongxiuwen61@gmail.com>
2021-06-05 12:10:48 +1000

    coding style fix

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, exceptions
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email')
+
 
 class SignupSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=20, min_length=6)
@@ -39,6 +41,7 @@ class SignupSerializer(serializers.ModelSerializer):
         )
         return user
 
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password= serializers.CharField()
+    password = serializers.CharField()
